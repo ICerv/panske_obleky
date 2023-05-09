a12efcff181d258216c5cf39f6f57335d4d5ba72
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-05-09 20:17:49 +0200

    Pridani tlacitek s barvama

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log('funguju');
+
+const shirt = document.querySelector('svg');
+
+document.querySelector('.colors').innerHTML += `
+<div class="color">
+            <div class="color-box color-box--black"></div>
+            <p class="color__name">černá</p>
+          </div>
+          <div class="color">
+            <div class="color-box color-box--red"></div>
+            <p class="color__name">červená</p>
+          </div>
+          <div class="color">
+            <div class="color-box color-box--blue"></div>
+            <p class="color__name">nebesky modrá</p>
+          </div>`;
+
+const colors = document.querySelectorAll('.color-box');
+
+for (let i = 0; i < colors.length; i++) {
+  colors[i].addEventListener('click', () => {
+    shirt.style = `fill : ${getComputedStyle(colors[i]).backgroundColor}`;
+  });
+}
